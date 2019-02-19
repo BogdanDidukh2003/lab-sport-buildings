c2be5a5dd8263bdbae93ad3c1532961e8124f9e6
Denys Shutka <shine.at.dusk@gmail.com>
2019-02-19 18:35:49 +0200

    changed to maven structure

---
+++ src/main/java/ua/lviv/iot/sportBuildings/models/AquaticsHall.java
@@ -0,0 +1,46 @@
+package ua.lviv.iot.sportBuildings.models;
+
+public class AquaticsHall extends SportBuilding {
+
+    private int poolsNumber;
+    private double averagePoolVolume;
+
+    public AquaticsHall() {}
+
+    public AquaticsHall(int poolsNumber, double averagePoolVolume) {
+        this.poolsNumber = poolsNumber;
+        this.averagePoolVolume = averagePoolVolume;
+    }
+
+    public AquaticsHall(String name, String location, int constructionYear, SportSeason sportSeason,
+                        int viewersNumber, SportKind sportKind, int poolsNumber, double averagePoolVolume) {
+        super(name, location, constructionYear, sportSeason, viewersNumber, sportKind);
+        this.poolsNumber = poolsNumber;
+        this.averagePoolVolume = averagePoolVolume;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + "AquaticsHall{" +
+                "poolsNumber=" + poolsNumber +
+                ", averagePoolVolume=" + averagePoolVolume +
+                '}';
+    }
+
+    public int getPoolsNumber() {
+        return poolsNumber;
+    }
+
+    public void setPoolsNumber(int poolsNumber) {
+        this.poolsNumber = poolsNumber;
+    }
+
+    public double getAveragePoolVolume() {
+        return averagePoolVolume;
+    }
+
+    public void setAveragePoolVolume(double averagePoolVolume) {
+        this.averagePoolVolume = averagePoolVolume;
+    }
+}
