25b8370b9a347b13f6f4db28e6889076ecdd0a4e
Denys Shutka <shine.at.dusk@gmail.com>
2019-02-21 20:50:39 +0200

    added tests

---
+++ src/test/java/ua/lviv/iot/sportBuildings/MainTest.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.sportBuildings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testCreateSportBuildingsList() {
+        assertNotNull(Main.createSportBuildingsList());
+    }
+}
